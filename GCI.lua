101bb2ed6df799bade4d5bae0a65cedc86955cb0
QuaterRG <vka419@gmail.com>
2024-10-11 18:35:28 +0300

    Update GCI.lua

--- GCI.lua
+++ GCI.lua
@@ -1,5 +1,5 @@
 function runBasicCut()
-	while wait(0.5) do -- Перемещено сюда 
+	while wait(0.5) do
 		local args = {
 			[1] = {
 				["id"] = "cut",
@@ -12,7 +12,7 @@ function runBasicCut()
 	end
 end
 function runIridiumCut()
-	while wait(0.05) do -- Перемещено сюда
+	while wait(0.05) do
 		local args = {
 			[1] = {
 				["id"] = "cut",
@@ -25,7 +25,7 @@ function runIridiumCut()
 	end
 end
 function runMushCut()
-	while wait(0.5) do -- Перемещено сюда
+	while wait(0.5) do
 		local args = {
 			[1] = {
 				["id"] = "cut",
@@ -50,6 +50,19 @@ function runUpg()
 			game:GetService("ReplicatedStorage").Remotes.GenericEvent:FireServer(unpack(args))
 			wait(0.5)
 		end
+	end
+end
+function runIridiumCut()
+	while wait(0.05) do
+		local args = {
+			[1] = {
+				["id"] = "cut",
+				["identifier"] = "rune",
+				["quantity"] = 0.0000000000000001,
+				["field"] = "cavern"
+			}
+		} 
+		game:GetService("ReplicatedStorage").Remotes.GenericFunction:InvokeServer(unpack(args))
 	end
 end
 coroutine.wrap(runBasicCut)()
