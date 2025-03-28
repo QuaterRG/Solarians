34da5d841be23d09d50592d848b038c3bfb5d0c5
QuaterRG <vka419@gmail.com>
2025-03-28 19:27:38 +0300

    Update Tower.lua

--- Tower.lua
+++ Tower.lua
@@ -1,6 +1,6 @@
 function T3()
   local args = {
-   {
+    {
     id = "cut",
     identifier = "pw8",
     quantity = 100,
@@ -8,6 +8,8 @@ function T3()
    }
   }
   game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GenericFunction"):InvokeServer(unpack(args))
+  end
+end
 function T2P()
   local args = {
    {
@@ -18,6 +20,8 @@ function T2P()
    }
   }
   game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GenericFunction"):InvokeServer(unpack(args))
+  end
+end
 function T3P()
   local args = {
    {
@@ -28,6 +32,8 @@ function T3P()
    }
   }
   game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GenericFunction"):InvokeServer(unpack(args))
+  end
+end
 while wait(0.1) do
   coroutine.wrap(T3)()
   coroutine.wrap(T2P)()
