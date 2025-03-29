6eab12018d6a9b5a041d3c4eec2d729ca8d729ab
QuaterRG <vka419@gmail.com>
2025-03-29 23:35:42 +0300

    Update W1.lua

--- W1.lua
+++ W1.lua
@@ -1,32 +1,32 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local GenericFunction = ReplicatedStorage.Remotes.GenericFunction
-local GenericEvent = ReplicatedStorage.Remotes.GenericEvent
 
-local function invokeCut(quantity, field)
+local function invokeCut(identifier, quantity, field)
     local args = {
         [1] = {
             ["id"] = "cut",
-            ["identifier"] = "pw8",
+            ["identifier"] = identifier,
             ["quantity"] = quantity,
             ["field"] = field
         }
     }
     GenericFunction:InvokeServer(unpack(args))
 end
-local function createCutFunction(quantity, field)
+
+local function createCutFunction(identifier, quantity, field)
     return function()
         while true do
-            invokeCut(quantity, field)
+            invokeCut(identifier, quantity, field)
             wait(0.05) 
         end
     end
 end
 
-coroutine.wrap(createCutFunction(1000000000000, "main"))()
-coroutine.wrap(createCutFunction(-10000000000000, "main"))()
-coroutine.wrap(createCutFunction(1000000000000, "anti"))()
-coroutine.wrap(createCutFunction(-10000000000000, "anti"))()
-coroutine.wrap(createCutFunction(1000000000000, "un"))()
-coroutine.wrap(createCutFunction(-10000000000000, "un"))()
-coroutine.wrap(createCutFunction(1000000000000, "planet"))()
-coroutine.wrap(createCutFunction(-10000000000000, "planet"))()
+coroutine.wrap(createCutFunction("pw8", 1000000000000, "main"))()
+coroutine.wrap(createCutFunction("pw8", -10000000000000, "main"))()
+coroutine.wrap(createCutFunction("pw8", 1000000000000, "anti"))()
+coroutine.wrap(createCutFunction("pw8", -10000000000000, "anti"))()
+coroutine.wrap(createCutFunction("pw8", 1000000000000, "un"))()
+coroutine.wrap(createCutFunction("pw8", -10000000000000, "un"))()
+coroutine.wrap(createCutFunction("pw8", 1000000000000, "planet"))()
+coroutine.wrap(createCutFunction("pw8", -10000000000000, "planet"))()
