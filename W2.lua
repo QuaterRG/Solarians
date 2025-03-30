da550db8cc39e4cebf573820cd825523bd1863a8
QuaterRG <vka419@gmail.com>
2025-03-30 19:15:13 +0300

    Update W2.lua

--- W2.lua
+++ W2.lua
@@ -14,26 +14,11 @@ local function invokeCut(identifier, quantity, field)
     GenericFunction:InvokeServer(unpack(args))
 end
 
-local function Upg()
-    while wait(0.5) do
-        for _, upg in ipairs({"2_ssfSC", "2_ssfUTXP", "2_ssfLR", "2_ssftap", "2_ssfCO", "2_ssfsf", "2_ssfTC", "2_ssfME", "2_scSSF", "2_scDXP", "2_scssp", "2_scbo", "2_scCT", "2_scCTXP", "2_scTE"}) do
-            local args = {
-                [1] = {
-                    ["id"] = "buyUpgrade",
-                    ["mode"] = "max",
-                    ["upgradeId"] = upg
-                }
-            }
-            GenericEvent:FireServer(unpack(args))
-        end
-    end
-end
-
 local function createCutFunction(identifier, quantity, field)
     return function()
         while true do
             invokeCut(identifier, quantity, field)
-            wait(0.5) 
+            wait(0.1) 
         end
     end
 end
@@ -45,7 +30,6 @@ coroutine.wrap(createCutFunction("ruby", -100000000000000000, "jungle"))()
 coroutine.wrap(createCutFunction("fractal", 10000000000000000, "rift"))()
 coroutine.wrap(createCutFunction("fractal", -100000000000000000, "rift"))()
 coroutine.wrap(createCutFunction("fractal", 10000000000000000, "cavern"))()
-coroutine.wrap(Upg)() 
 coroutine.wrap(createCutFunction("rune", -100000000000000000, "cavern"))()
 coroutine.wrap(createCutFunction("fractal", 100000000000000000, "desert"))()
 coroutine.wrap(createCutFunction("platinum", -100000000000000000, "desert"))()
