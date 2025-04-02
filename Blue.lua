951360991ad9c36bf7217d7362fcbe27e1722203
QuaterRG <vka419@gmail.com>
2025-04-02 17:40:11 +0300

    Update Blue.lua

--- Blue.lua
+++ Blue.lua
@@ -1,10 +1,15 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
 local Blue = Instance.new("ScreenGui")
 local BlueMenu = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
 local Blue_2 = Instance.new("ImageLabel")
 local AutoCut = Instance.new("TextButton")
-local UIGradient = Instance.new("UIGradient")
 local UICorner_2 = Instance.new("UICorner")
+local UIGradient = Instance.new("UIGradient")
 local AutoBuy = Instance.new("TextButton")
 local UIGradient_2 = Instance.new("UIGradient")
 local UICorner_3 = Instance.new("UICorner")
@@ -21,10 +26,9 @@ BlueMenu.Parent = Blue
 BlueMenu.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 BlueMenu.BorderColor3 = Color3.fromRGB(0, 0, 0)
 BlueMenu.BorderSizePixel = 0
-BlueMenu.Position = UDim2.new(0.912423909, 0, 0.275981516, 0)
+BlueMenu.Position = UDim2.new(0.911158085, 0, 0.467667431, 0)
 BlueMenu.Size = UDim2.new(0.0828226134, 0, 0.150115475, 0)
 BlueMenu.Image = "rbxassetid://121567489138361"
-BlueMenu.ImageTransparency = 0.100
 
 UICorner.CornerRadius = UDim.new(74574, 434324)
 UICorner.Parent = BlueMenu
@@ -34,11 +38,10 @@ Blue_2.Parent = Blue
 Blue_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Blue_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Blue_2.BorderSizePixel = 0
-Blue_2.Position = UDim2.new(0.668987334, 0, 0.224018469, 0)
+Blue_2.Position = UDim2.new(0.668987334, 0, 0.398383379, 0)
 Blue_2.Size = UDim2.new(0.189873412, 0, 0.202078521, 0)
 Blue_2.Visible = false
 Blue_2.Image = "rbxassetid://99401546465208"
-Blue_2.ImageTransparency = 0.150
 
 AutoCut.Name = "AutoCut"
 AutoCut.Parent = Blue_2
@@ -52,15 +55,14 @@ AutoCut.Text = "Cut"
 AutoCut.TextColor3 = Color3.fromRGB(0, 0, 0)
 AutoCut.TextScaled = true
 AutoCut.TextSize = 60.000
-AutoCut.TextTransparency = 0.150
 AutoCut.TextWrapped = true
+
+UICorner_2.CornerRadius = UDim.new(0, 40)
+UICorner_2.Parent = AutoCut
 
 UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(164, 199, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(164, 199, 255))}
 UIGradient.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 0.44), NumberSequenceKeypoint.new(0.20, 0.28), NumberSequenceKeypoint.new(0.50, 0.36), NumberSequenceKeypoint.new(0.77, 0.27), NumberSequenceKeypoint.new(1.00, 0.41)}
 UIGradient.Parent = AutoCut
-
-UICorner_2.CornerRadius = UDim.new(0, 40)
-UICorner_2.Parent = AutoCut
 
 AutoBuy.Name = "AutoBuy"
 AutoBuy.Parent = Blue_2
@@ -74,7 +76,6 @@ AutoBuy.Text = "Buy"
 AutoBuy.TextColor3 = Color3.fromRGB(0, 0, 0)
 AutoBuy.TextScaled = true
 AutoBuy.TextSize = 40.000
-AutoBuy.TextTransparency = 0.150
 AutoBuy.TextWrapped = true
 
 UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(164, 199, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(164, 199, 255))}
@@ -89,7 +90,7 @@ UICorner_4.Parent = Blue_2
 
 -- Scripts:
 
-local function IYVHQUS_fake_script() -- BlueMenu.Script 
+local function GKYBK_fake_script() -- BlueMenu.Script 
 	local script = Instance.new('Script', BlueMenu)
 
 	local Button = script.Parent
@@ -105,8 +106,8 @@ local function IYVHQUS_fake_script() -- BlueMenu.Script
 	end
 	Button.MouseButton1Click:Connect(onClick)
 end
-coroutine.wrap(IYVHQUS_fake_script)()
-local function FJDN_fake_script() -- AutoCut.Script 
+coroutine.wrap(GKYBK_fake_script)()
+local function LVWWO_fake_script() -- AutoCut.Script 
 	local script = Instance.new('Script', AutoCut)
 
 	local isRunning = false
@@ -115,7 +116,7 @@ local function FJDN_fake_script() -- AutoCut.Script
 	local RedColor = Color3.new(0.521569, 0.486275, 1)
 	
 	local function runBasicCut()
-		while isRunning do -- Перемещено сюда
+		while isRunning do
 			local args = {
 				[1] = {
 					["id"] = "cut",
@@ -128,7 +129,7 @@ local function FJDN_fake_script() -- AutoCut.Script
 		end
 	end
 	local function runIridiumCut()
-		while isRunning do -- Перемещено сюда
+		while isRunning do
 			local args = {
 				[1] = {
 					["id"] = "cut",
@@ -155,8 +156,8 @@ local function FJDN_fake_script() -- AutoCut.Script
 		end
 	end)
 end
-coroutine.wrap(FJDN_fake_script)()
-local function STLC_fake_script() -- AutoBuy.Script 
+coroutine.wrap(LVWWO_fake_script)()
+local function WBDSSXH_fake_script() -- AutoBuy.Script 
 	local script = Instance.new('Script', AutoBuy)
 
 	local isRunning = false
@@ -166,16 +167,20 @@ local function STLC_fake_script() -- AutoBuy.Script
 	
 	local function startLoop()
 		while isRunning do
-			for _, upg in ipairs({"br1", "br2", "br4", "ir5"}) do
-				local args = {
-					[1] = {
-						["id"] = "buyUpgrade",
-						["mode"] = "max",
-						["upgradeId"] = upg
-					}
-				}
-				game:GetService("ReplicatedStorage").Remotes.GenericEvent:FireServer(unpack(args))
-				wait(0.4)
+			for _, upgrade in ipairs(game:GetService("Players").LocalPlayer.PlayerGui.W1.Brass.ShopContainer.ScrollingFrame:GetChildren()) do
+				if upgrade.Name == "BasicUpgrade" then
+					if upgrade.Display.CostDisplay.Text:find("#00ff00") then
+						local args = {
+							[1] = {
+								["id"] = "buyUpgrade",
+								["mode"] = "max",
+								["upgradeId"] = upgrade.Variables.Identifier.Value
+							}
+						}
+						game:GetService("ReplicatedStorage").Remotes.GenericEvent:FireServer(unpack(args))
+						wait(1)
+					end
+				end
 			end
 			wait(5)
 		end
@@ -191,4 +196,4 @@ local function STLC_fake_script() -- AutoBuy.Script
 		end
 	end)
 end
-coroutine.wrap(STLC_fake_script)()
+coroutine.wrap(WBDSSXH_fake_script)()
