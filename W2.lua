e6f268575ca2db2b6d2d840fb4371a8611caf5e1
QuaterRG <vka419@gmail.com>
2025-04-06 11:05:34 +0300

    Update W2.lua

--- W2.lua
+++ W2.lua
@@ -1,6 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local GenericFunction = ReplicatedStorage.Remotes.GenericFunction
-local GenericEvent = ReplicatedStorage.Remotes.GenericEvent
 
 local function invokeCut(identifier, quantity, field)
     GenericFunction:InvokeServer({["id"] = "cut", ["identifier"] = identifier, ["quantity"] = quantity, ["field"] = field})
@@ -15,21 +14,29 @@ local function createCutFunction(identifier, quantity, field)
     end
 end
 
-coroutine.wrap(createCutFunction("fractal", 1e18, "intersection"))()
+-- Группируем вызовы по полям и меняем порядок
 coroutine.wrap(createCutFunction("fractal", -1e18, "intersection"))()
-coroutine.wrap(createCutFunction("fractal", 1e18, "jungle"))()
+coroutine.wrap(createCutFunction("fractal", 1e18, "intersection"))()
+
 coroutine.wrap(createCutFunction("ruby", -1e18, "jungle"))()
-coroutine.wrap(createCutFunction("fractal", 1e18, "rift"))()
+coroutine.wrap(createCutFunction("fractal", 1e18, "jungle"))()
+
 coroutine.wrap(createCutFunction("fractal", -1e18, "rift"))()
-coroutine.wrap(createCutFunction("fractal", 1e18, "cavern"))()
+coroutine.wrap(createCutFunction("fractal", 1e18, "rift"))()
+
 coroutine.wrap(createCutFunction("rune", -1e18, "cavern"))()
-coroutine.wrap(createCutFunction("fractal", 1e18, "desert"))()
+coroutine.wrap(createCutFunction("fractal", 1e18, "cavern"))()
+
 coroutine.wrap(createCutFunction("platinum", -1e18, "desert"))()
 coroutine.wrap(createCutFunction("moonstone", -1e18, "desert"))()
-coroutine.wrap(createCutFunction("fractal", 1e18, "temple"))()
+coroutine.wrap(createCutFunction("fractal", 1e18, "desert"))()
+
 coroutine.wrap(createCutFunction("platinum", -1e18, "temple"))()
 coroutine.wrap(createCutFunction("moonstone", -1e18, "temple"))()
-coroutine.wrap(createCutFunction("pw8", 1e18, "cactus"))()
+coroutine.wrap(createCutFunction("fractal", 1e18, "temple"))()
+
 coroutine.wrap(createCutFunction("platinum", -1e18, "cactus"))()
-coroutine.wrap(createCutFunction("fractal", 1e18, "understand"))()
+coroutine.wrap(createCutFunction("pw8", 1e18, "cactus"))()
+
 coroutine.wrap(createCutFunction("fractal", -1e18, "understand"))()
+coroutine.wrap(createCutFunction("fractal", 1e18, "understand"))()
