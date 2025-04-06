242a81b9e210613162510a1debda3d35e1d7f0e7
QuaterRG <vka419@gmail.com>
2025-04-06 13:23:11 +0300

    Update Tower.lua

--- Tower.lua
+++ Tower.lua
@@ -1,12 +1,11 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local GenericFunction = ReplicatedStorage.Remotes.GenericFunction
-
-local function cutField(identifier, quantity, field)
+local GenericFunction = game:GetService("ReplicatedStorage").Remotes.GenericFunction
+local fields = {"9_F1", "9_F2", "9_F3", "9_F4", "9_F5"}
+local function cutField(quantity, field)
     local args = {
         [1] = {
             ["id"] = "cut",
             ["quantity"] = quantity,
-            ["identifier"] = identifier,
+            ["identifier"] = "pw8",
             ["field"] = field
         }
     }
@@ -16,24 +15,21 @@ end
 local function cutPlatinum(field)
     local args = {
         [1] = {
-            id = "cut",
-            identifier = "platinum",
-            quantity = -100000000,
-            field = field
+            ["id"] = "cut",
+            ["quantity"] = -1e8,
+            ["identifier"] = "platinum",
+            ["field"] = field
         }
     }
     GenericFunction:InvokeServer(unpack(args))
 end
 
 local function performCuts()
-    local fields = {"9_F1", "9_F2", "9_F3", "9_F4", "9_F5"}
     for _, field in ipairs(fields) do
-        cutField("pw8", 1000000, field)
-        cutField("pw8", -10000000, field)
+        cutField(-1e8, field)
+        cutField(1e8, field)
     end
-    for i = 3, 5 do
-        cutPlatinum("9_F" .. i)
-    end
+    cutPlatinum("9_F5")
 end
 
 while wait(0.02) do
