f0c1e0264419d371a89d212c3a3be7fb53f68290
QuaterRG <vka419@gmail.com>
2025-04-08 07:49:22 +0300

    Update W1.lua

--- W1.lua
+++ W1.lua
@@ -9,21 +9,19 @@ local UICorner = Instance.new("UICorner")
 local UIGradient = Instance.new("UIGradient")
 local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 local Menu = Instance.new("Frame")
-local AutoSoul3 = Instance.new("TextButton")
+local AutoSoul3upg = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
-local AutoShard = Instance.new("TextButton")
+local AutoResetShard = Instance.new("TextButton")
 local UICorner_3 = Instance.new("UICorner")
-local AutoReset = Instance.new("TextButton")
-local UICorner_4 = Instance.new("UICorner")
 local UIGradient_2 = Instance.new("UIGradient")
-local AutoUpgrade = Instance.new("TextButton")
-local UICorner_5 = Instance.new("UICorner")
-local UICorner_6 = Instance.new("UICorner")
+local UICorner_4 = Instance.new("UICorner")
 local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
 local AutoCentr = Instance.new("TextButton")
-local UICorner_7 = Instance.new("UICorner")
+local UICorner_5 = Instance.new("UICorner")
 local AutoCut = Instance.new("TextButton")
-local UICorner_8 = Instance.new("UICorner")
+local UICorner_6 = Instance.new("UICorner")
+local AutoSoul3upg_2 = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
 
 --Properties:
 
@@ -65,80 +63,46 @@ Menu.Position = UDim2.new(0.634230077, 0, 0.182844833, 0)
 Menu.Size = UDim2.new(0.215867281, 0, 0.370094717, 0)
 Menu.Visible = false
 
-AutoSoul3.Name = "AutoSoul3"
-AutoSoul3.Parent = Menu
-AutoSoul3.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-AutoSoul3.BorderColor3 = Color3.fromRGB(0, 0, 0)
-AutoSoul3.BorderSizePixel = 0
-AutoSoul3.Position = UDim2.new(0.0499998555, 0, 0.0938634574, 0)
-AutoSoul3.Size = UDim2.new(0.266666681, 0, 0.362167358, 0)
-AutoSoul3.Font = Enum.Font.SourceSans
-AutoSoul3.Text = "Soul3"
-AutoSoul3.TextColor3 = Color3.fromRGB(0, 0, 0)
-AutoSoul3.TextScaled = true
-AutoSoul3.TextSize = 40.000
-AutoSoul3.TextWrapped = true
+AutoSoul3upg.Name = "AutoSoul3+upg"
+AutoSoul3upg.Parent = Menu
+AutoSoul3upg.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+AutoSoul3upg.BorderColor3 = Color3.fromRGB(0, 0, 0)
+AutoSoul3upg.BorderSizePixel = 0
+AutoSoul3upg.Position = UDim2.new(0.0499998555, 0, 0.0938634574, 0)
+AutoSoul3upg.Size = UDim2.new(0.266666681, 0, 0.362167358, 0)
+AutoSoul3upg.Font = Enum.Font.SourceSans
+AutoSoul3upg.Text = " Soul3 + upg"
+AutoSoul3upg.TextColor3 = Color3.fromRGB(0, 0, 0)
+AutoSoul3upg.TextScaled = true
+AutoSoul3upg.TextSize = 40.000
+AutoSoul3upg.TextWrapped = true
 
 UICorner_2.CornerRadius = UDim.new(0, 70)
-UICorner_2.Parent = AutoSoul3
+UICorner_2.Parent = AutoSoul3upg
 
-AutoShard.Name = "AutoShard"
-AutoShard.Parent = Menu
-AutoShard.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-AutoShard.BorderColor3 = Color3.fromRGB(0, 0, 0)
-AutoShard.BorderSizePixel = 0
-AutoShard.Position = UDim2.new(0.366666943, 0, 0.0938634574, 0)
-AutoShard.Size = UDim2.new(0.266666681, 0, 0.36216718, 0)
-AutoShard.Font = Enum.Font.SourceSans
-AutoShard.Text = "Shard"
-AutoShard.TextColor3 = Color3.fromRGB(0, 0, 0)
-AutoShard.TextScaled = true
-AutoShard.TextSize = 40.000
-AutoShard.TextWrapped = true
+AutoResetShard.Name = "AutoReset+Shard"
+AutoResetShard.Parent = Menu
+AutoResetShard.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+AutoResetShard.BorderColor3 = Color3.fromRGB(0, 0, 0)
+AutoResetShard.BorderSizePixel = 0
+AutoResetShard.Position = UDim2.new(0.683333278, 0, 0.0938633978, 0)
+AutoResetShard.Size = UDim2.new(0.266666859, 0, 0.362167269, 0)
+AutoResetShard.Font = Enum.Font.SourceSans
+AutoResetShard.Text = "AutoReset shard"
+AutoResetShard.TextColor3 = Color3.fromRGB(0, 0, 0)
+AutoResetShard.TextScaled = true
+AutoResetShard.TextSize = 40.000
+AutoResetShard.TextWrapped = true
 
 UICorner_3.CornerRadius = UDim.new(0, 70)
-UICorner_3.Parent = AutoShard
-
-AutoReset.Name = "AutoReset"
-AutoReset.Parent = Menu
-AutoReset.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-AutoReset.BorderColor3 = Color3.fromRGB(0, 0, 0)
-AutoReset.BorderSizePixel = 0
-AutoReset.Position = UDim2.new(0.683333278, 0, 0.0938633978, 0)
-AutoReset.Size = UDim2.new(0.266666859, 0, 0.362167269, 0)
-AutoReset.Font = Enum.Font.SourceSans
-AutoReset.Text = "AutoReset"
-AutoReset.TextColor3 = Color3.fromRGB(0, 0, 0)
-AutoReset.TextScaled = true
-AutoReset.TextSize = 40.000
-AutoReset.TextWrapped = true
-
-UICorner_4.CornerRadius = UDim.new(0, 70)
-UICorner_4.Parent = AutoReset
+UICorner_3.Parent = AutoResetShard
 
 UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 238, 0)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 238, 0))}
 UIGradient_2.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 0.21), NumberSequenceKeypoint.new(0.10, 0.49), NumberSequenceKeypoint.new(0.49, 0.26), NumberSequenceKeypoint.new(0.83, 0.52), NumberSequenceKeypoint.new(1.00, 0.16)}
 UIGradient_2.Parent = Menu
 
-AutoUpgrade.Name = "AutoUpgrade"
-AutoUpgrade.Parent = Menu
-AutoUpgrade.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-AutoUpgrade.BorderColor3 = Color3.fromRGB(0, 0, 0)
-AutoUpgrade.BorderSizePixel = 0
-AutoUpgrade.Position = UDim2.new(0.0499998555, 0, 0.568369746, 0)
-AutoUpgrade.Size = UDim2.new(0.266666681, 0, 0.362300634, 0)
-AutoUpgrade.Font = Enum.Font.SourceSans
-AutoUpgrade.Text = "Upgrade"
-AutoUpgrade.TextColor3 = Color3.fromRGB(0, 0, 0)
-AutoUpgrade.TextScaled = true
-AutoUpgrade.TextSize = 40.000
-AutoUpgrade.TextWrapped = true
-
-UICorner_5.CornerRadius = UDim.new(0, 70)
-UICorner_5.Parent = AutoUpgrade
-
-UICorner_6.CornerRadius = UDim.new(0, 30)
-UICorner_6.Parent = Menu
+UICorner_4.CornerRadius = UDim.new(0, 30)
+UICorner_4.Parent = Menu
 
 UIAspectRatioConstraint_2.Parent = Menu
 UIAspectRatioConstraint_2.AspectRatio = 1.360
@@ -157,15 +121,15 @@ AutoCentr.TextScaled = true
 AutoCentr.TextSize = 40.000
 AutoCentr.TextWrapped = true
 
-UICorner_7.CornerRadius = UDim.new(0, 70)
-UICorner_7.Parent = AutoCentr
+UICorner_5.CornerRadius = UDim.new(0, 70)
+UICorner_5.Parent = AutoCentr
 
 AutoCut.Name = "AutoCut"
 AutoCut.Parent = Menu
 AutoCut.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
 AutoCut.BorderColor3 = Color3.fromRGB(0, 0, 0)
 AutoCut.BorderSizePixel = 0
-AutoCut.Position = UDim2.new(0.366666943, 0, 0.568369746, 0)
+AutoCut.Position = UDim2.new(0.366666883, 0, 0.092428796, 0)
 AutoCut.Size = UDim2.new(0.266666681, 0, 0.362167269, 0)
 AutoCut.Font = Enum.Font.SourceSans
 AutoCut.Text = "AutoCut"
@@ -174,12 +138,29 @@ AutoCut.TextScaled = true
 AutoCut.TextSize = 40.000
 AutoCut.TextWrapped = true
 
-UICorner_8.CornerRadius = UDim.new(0, 70)
-UICorner_8.Parent = AutoCut
+UICorner_6.CornerRadius = UDim.new(0, 70)
+UICorner_6.Parent = AutoCut
+
+AutoSoul3upg_2.Name = "AutoSoul3+upg"
+AutoSoul3upg_2.Parent = Menu
+AutoSoul3upg_2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+AutoSoul3upg_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+AutoSoul3upg_2.BorderSizePixel = 0
+AutoSoul3upg_2.Position = UDim2.new(0.0500000529, 0, 0.565770984, 0)
+AutoSoul3upg_2.Size = UDim2.new(0.266666681, 0, 0.362167358, 0)
+AutoSoul3upg_2.Font = Enum.Font.SourceSans
+AutoSoul3upg_2.Text = " Soul3 + upg"
+AutoSoul3upg_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+AutoSoul3upg_2.TextScaled = true
+AutoSoul3upg_2.TextSize = 40.000
+AutoSoul3upg_2.TextWrapped = true
+
+UICorner_7.CornerRadius = UDim.new(0, 70)
+UICorner_7.Parent = AutoSoul3upg_2
 
 -- Scripts:
 
-local function FLRP_fake_script() -- OpenMenu.Script 
+local function HFGILNG_fake_script() -- OpenMenu.Script 
 	local script = Instance.new('Script', OpenMenu)
 
 	local Button = script.Parent
@@ -195,126 +176,9 @@ local function FLRP_fake_script() -- OpenMenu.Script
 	end
 	Button.MouseButton1Click:Connect(onClick)
 end
-coroutine.wrap(FLRP_fake_script)()
-local function ZHIUAJ_fake_script() -- AutoSoul3.Script 
-	local script = Instance.new('Script', AutoSoul3)
-
-	local isRunning = false
-	local Button = script.Parent
-	local GreenColor = Color3.new(0, 1, 0)
-	local RedColor = Color3.new(1, 0, 0)
-	local event = game:GetService("ReplicatedStorage").Remotes.GenericEvent
-	
-	local function fireEvent(id, x)
-		event:FireServer({["id"] = id, ["x"] = x})
-	end
-	
-	local function startLoop()
-		while isRunning do
-			fireEvent("changeSoul", 3)
-			wait(0.2)
-			fireEvent("setSoul", 0.25)
-			wait(0.5)
-	
-			for _, buy in ipairs({"soul2", "offdef2", "syncs", "syngs"}) do
-				event:FireServer({["id"] = "buySoul", ["soulId"] = buy})
-				wait(0.2)
-			end
-			wait(10)
-		end
-	end
-	
-	Button.MouseButton1Click:Connect(function()
-		isRunning = not isRunning
-		Button.BackgroundColor3 = isRunning and GreenColor or RedColor
-	
-		if isRunning then
-			startLoop()
-		end
-	end)
-end
-coroutine.wrap(ZHIUAJ_fake_script)()
-local function JVONLTG_fake_script() -- AutoShard.Script 
-	local script = Instance.new('Script', AutoShard)
-
-	local isRunning = false
-	local Button = script.Parent
-	local GreenColor = Color3.new(0, 1, 0)
-	local RedColor = Color3.new(1, 0, 0)
-	local Scroll = game:GetService("Players").LocalPlayer.PlayerGui.W1.SythesisPlots.ShopContainer.ScrollingFrame:GetChildren()
-	local event = game:GetService("ReplicatedStorage").Remotes.GenericEvent
-	local paths = {}
-	
-	for _, child in ipairs(Scroll) do
-		if child.Name == "BasicUpgrade" then
-			table.insert(paths, child)
-		end
-	end
-	
-	local function startLoop()
-		while isRunning do
-			for _, child in ipairs(Scroll) do
-				if child.Name == "Plot" and child.Identifier.Value == 1 and child.Normal.PB.Progress.Text ~= "0s" then
-					event:FireServer({["id"] = "synthesis", ["selected"] = "cs", ["syn"] = 1})
-					break
-				end
-			end
-			wait(3)
-		end
-	end
-	
-	Button.MouseButton1Click:Connect(function()
-		isRunning = not isRunning
-	
-		if isRunning then
-			Button.BackgroundColor3 = GreenColor
-			coroutine.wrap(startLoop)() 
-		else
-			Button.BackgroundColor3 = RedColor
-		end
-	end)
-end
-coroutine.wrap(JVONLTG_fake_script)()
-local function JRLFO_fake_script() -- AutoReset.Script 
-	local script = Instance.new('Script', AutoReset)
-
-	local isRunning = false
-	local Button = script.Parent
-	local GreenColor = Color3.new(0, 1, 0)
-	local RedColor = Color3.new(1, 0, 0)
-	local gui = game:GetService("Players").LocalPlayer.PlayerGui
-	local reset = game:GetService("ReplicatedStorage").Remotes.TriggerReset
-	
-	local function startLoop()
-		while isRunning do
-			if gui.Solarians.Restoration.ShopContainer.Menus.StarTier.UpgradeSNT.Visible then
-				reset:FireServer("subparnova")
-				wait(1)
-			end
-			reset:FireServer("activateSynthesis")
-			wait(1)
-			if not gui.W1.BrokenRing.Locked.Visible and gui.W1.BrokenRing.Enabled then
-				reset:FireServer("breakRing")
-				wait(1)
-			end
-			wait(1)
-		end
-	end
-	
-	Button.MouseButton1Click:Connect(function()
-		isRunning = not isRunning
-	
-		if isRunning then
-			Button.BackgroundColor3 = GreenColor
-			coroutine.wrap(startLoop)() 
-		else
-			Button.BackgroundColor3 = RedColor
-		end
-	end)
-end
-coroutine.wrap(JRLFO_fake_script)()
-local function WGNSEMI_fake_script() -- AutoUpgrade.Script 
-	local script = Instance.new('Script', AutoUpgrade)
+coroutine.wrap(HFGILNG_fake_script)()
+local function YKWVT_fake_script() -- AutoSoul3upg.Script 
+	local script = Instance.new('Script', AutoSoul3upg)
 
 	local Button = script.Parent
 	local GreenColor = Color3.new(0, 1, 0)
@@ -332,7 +196,22 @@ local function WGNSEMI_fake_script() -- AutoUpgrade.Script
 		end
 	end
 	
-	local function startLoop()
+	local function soulLoop()
+		while isRunning do
+			event:FireServer({["id"] = "changeSoul", ["x"] = 3})
+			wait(0.2)
+			event:FireServer({["id"] = "setSoul", ["x"] = 0.25})
+			wait(0.5)
+	
+			for _, buy in ipairs({"soul2", "offdef2", "syncs", "syngs"}) do
+				event:FireServer({["id"] = "buySoul", ["soulId"] = buy})
+				wait(0.2)
+			end
+			wait(10)
+		end
+	end
+	
+	local function shopLoop()
 		while isRunning do
 			for _, unlock in ipairs(paths) do
 				if unlock.Level.Value == 0 and unlock.Unlocked.Button.Gradient.Cost.Text:find("#00ff00") then
@@ -348,12 +227,76 @@ local function WGNSEMI_fake_script() -- AutoUpgrade.Script
 		Button.BackgroundColor3 = isRunning and GreenColor or RedColor
 	
 		if isRunning then 
-			startLoop()
+			coroutine.wrap(soulLoop)()
+			coroutine.wrap(shopLoop)()
 		end 
-	end)	
+	end)
 end
-coroutine.wrap(WGNSEMI_fake_script)()
-local function RUJURKX_fake_script() -- AutoCentr.Script 
+coroutine.wrap(YKWVT_fake_script)()
+local function AKHI_fake_script() -- AutoResetShard.Script 
+	local script = Instance.new('Script', AutoResetShard)
+
+	local isRunning = false
+	local Button = script.Parent
+	local GreenColor = Color3.new(0, 1, 0)
+	local RedColor = Color3.new(1, 0, 0)
+	local gui = game:GetService("Players").LocalPlayer.PlayerGui
+	local reset = game:GetService("ReplicatedStorage").Remotes.TriggerReset
+	local Scroll = gui.W1.SythesisPlots.ShopContainer.ScrollingFrame:GetChildren()
+	local event = game:GetService("ReplicatedStorage").Remotes.GenericEvent
+	local paths = {}
+	
+	for _, child in ipairs(Scroll) do
+		if child.Name == "BasicUpgrade" then
+			table.insert(paths, child)
+		end
+	end
+	
+	local function restorationLoop()
+		while isRunning do
+			if gui.Solarians.Restoration.ShopContainer.Menus.StarTier.UpgradeSNT.Visible then
+				reset:FireServer("subparnova")
+				wait(1)
+			end
+	
+			reset:FireServer("activateSynthesis")
+			wait(1)
+	
+			if not gui.W1.BrokenRing.Locked.Visible and gui.W1.BrokenRing.Enabled then
+				reset:FireServer("breakRing")
+				wait(1)
+			end
+	
+			wait(3)
+		end
+	end
+	
+	local function synthesisLoop()
+		while isRunning do
+			for _, child in ipairs(Scroll) do
+				if child.Name == "Plot" and child.Identifier.Value == 1 and child.Normal.PB.Progress.Text ~= "0s" then
+					event:FireServer({["id"] = "synthesis", ["selected"] = "cs", ["syn"] = 1})
+					break
+				end
+			end
+			wait(3)
+		end
+	end
+	
+	Button.MouseButton1Click:Connect(function()
+		isRunning = not isRunning
+	
+		if isRunning then 
+			Button.BackgroundColor3 = GreenColor
+			coroutine.wrap(restorationLoop)()
+			coroutine.wrap(synthesisLoop)()
+		else
+			Button.BackgroundColor3 = RedColor
+		end 
+	end)
+end
+coroutine.wrap(AKHI_fake_script)()
+local function NFRZLYS_fake_script() -- AutoCentr.Script 
 	local script = Instance.new('Script', AutoCentr)
 
 	local Button = script.Parent
@@ -422,8 +365,8 @@ local function RUJURKX_fake_script() -- AutoCentr.Script
 		end 
 	end)
 end
-coroutine.wrap(RUJURKX_fake_script)()
-local function TTZHV_fake_script() -- AutoCut.Script 
+coroutine.wrap(NFRZLYS_fake_script)()
+local function CGSBWT_fake_script() -- AutoCut.Script 
 	local script = Instance.new('Script', AutoCut)
 
 	local isRunning = false
@@ -465,4 +408,40 @@ local function TTZHV_fake_script() -- AutoCut.Script
 		end
 	end)
 end
-coroutine.wrap(TTZHV_fake_script)()
+coroutine.wrap(CGSBWT_fake_script)()
+local function BTMYVZE_fake_script() -- AutoSoul3upg_2.Script 
+	local script = Instance.new('Script', AutoSoul3upg_2)
+
+	local Button = script.Parent
+	local GreenColor = Color3.new(0, 1, 0)
+	local RedColor = Color3.new(1, 0, 0)
+	local isRunning = false
+	local shops = game:GetService("Players").LocalPlayer.PlayerGui.W1.SingularityChart.ShopContainer.Shops
+	local paths = {}
+	local event = game:GetService("ReplicatedStorage").Remotes.GenericEvent
+	
+	for _, shop in ipairs({shops.MagicShop, shops.SoulShop, shops.UnstableShop}) do
+		for _, child in ipairs(shop:GetChildren()) do
+			if child.Name == "Unlock" then
+				table.insert(paths, child)
+			end
+		end
+	end
+	
+	local function Loop()
+		while isRunning do
+			 -- placeholder
+		end
+	end
+	
+	
+	Button.MouseButton1Click:Connect(function()
+		isRunning = not isRunning
+		Button.BackgroundColor3 = isRunning and GreenColor or RedColor
+	
+		if isRunning then 
+			Loop()
+		end 
+	end)
+end
+coroutine.wrap(BTMYVZE_fake_script)()
